6dfa1c8f4b4572cf57678e53e7521c1af2fb5ff7
cop1fab <bienaimecopain@gmail.com>
2019-11-07 13:38:37 +0200

    added linting and changed es5 to es6

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-const express = require( "express" );
+import express from "express";
+
 const app = express();
 const port = 8080; // default port to listen
 
@@ -8,6 +9,8 @@ app.get( "/", ( req, res ) => {
 } );
 
 // start the Express server
+
 app.listen( port, () => {
+// tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
 } );
